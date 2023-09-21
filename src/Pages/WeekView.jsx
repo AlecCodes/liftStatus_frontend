1ba0cb8e1b3a8cc412c7a196f2a63cce2a990c71
Alec Hannaford <alec322@gmail.com>
2023-09-20 23:07:50 -0700

    week range displays dates

--- src/Pages/WeekView.jsx
+++ src/Pages/WeekView.jsx
@@ -4,8 +4,34 @@ import getUTCDateRange from "../Functions/dayRange";
 function WeekView(props){
     const {start, end} = useParams()
     console.log('daterange', getUTCDateRange(start, end))
+    const days = getUTCDateRange(start, end)
  
-    return <h1>WeekView!!</h1>
+    return (
+        <table border='solid'>
+            <tr>
+                {days.map((element, index) => (
+                            <td 
+                            key = {index} 
+                            ISO = {element} 
+                            >{element.toDateString()}</td>
+                        )
+                    )
+                }
+
+            </tr>
+            {/* <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr> */}
+
+        </table>
+
+    )
 }
 
 export default WeekView;
