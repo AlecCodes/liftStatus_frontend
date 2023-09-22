77e6b238d87aca8caa615463c5ad197cc1716c79
Alec Hannaford <alec322@gmail.com>
2023-09-21 23:48:40 -0700

    calendar starts on right day

--- src/Pages/MonthView.jsx
+++ src/Pages/MonthView.jsx
@@ -6,15 +6,33 @@ function MonthView(props){
     const {year, month} = useParams()
     const daysInMonthList = daysInMonth(year, month)
 
-    console.log(daysInMonthList)
+    //this aligns the start of the month on the proper day of week on our calendar
+    const firstDay = daysInMonthList[0].getDay()
+    const leadingBlankDays = []
+    for (let i = 0; i < firstDay; i++){
+        leadingBlankDays.push(<li></li>)
+    }
+
 
     //We use CSS
     return (
         <>
             <h1>MONTH VIEW!!</h1>
+            <ul className="weekdays">
+                <li>Sun</li>
+                <li>Mon</li>
+                <li>Tue</li>
+                <li>Wed</li>
+                <li>Thu</li>
+                <li>Fri</li>
+                <li>Sat</li>
+            </ul>
             <ul className="days">
+                {leadingBlankDays}
                 {daysInMonthList.map((date, index) => {
-                    return(<li>{date.toLocaleDateString()}</li>)
+                    return(<li
+                    key={index}
+                    >{date.toLocaleDateString()}</li>)
                 })}
             </ul>
         </>
