03a2229c85530807ab8dae06f90f0b6ce66cb1fa
Alec Hannaford <alec322@gmail.com>
2023-09-21 23:28:32 -0700

    grid of dates on month view

--- src/Pages/MonthView.jsx
+++ src/Pages/MonthView.jsx
@@ -4,13 +4,20 @@ import { useParams } from "react-router-dom"
 function MonthView(props){
 
     const {year, month} = useParams()
+    const daysInMonthList = daysInMonth(year, month)
 
-    console.log(daysInMonth(year, month))
+    console.log(daysInMonthList)
 
+    //We use CSS
     return (
-    <>
-        <h1>MONTH VIEW!!</h1>
-    </>
+        <>
+            <h1>MONTH VIEW!!</h1>
+            <ul className="days">
+                {daysInMonthList.map((date, index) => {
+                    return(<li>{date.toLocaleDateString()}</li>)
+                })}
+            </ul>
+        </>
     )
 }
 
