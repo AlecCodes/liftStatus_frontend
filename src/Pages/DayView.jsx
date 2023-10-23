65cfba0932ad9549313649697998105f9b20e458
Alec Hannaford <alec322@gmail.com>
2023-10-22 17:24:30 -0700

    dates rendered to day component

--- src/Pages/DayView.jsx
+++ src/Pages/DayView.jsx
@@ -6,6 +6,14 @@ function DayView(props){
     return (
         <>
             <h1>DAY VIEW!</h1>
+            <ul>
+            {data.map((element, index) => {
+                for (let i in Object.keys(element.chairs)){
+                    const mountainTime = new Date(data[index].reportDate)
+                    return <li key ={index}>{mountainTime.toLocaleString()}</li>
+                }
+            })}
+            </ul>
         </>
     )
 }
