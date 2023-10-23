b9e37db4cf46425bb3c7ff7d68808a4e67c14902
Alec Hannaford <alec322@gmail.com>
2023-10-22 17:50:35 -0700

    table html instead of ul

--- src/Pages/DayView.jsx
+++ src/Pages/DayView.jsx
@@ -2,18 +2,25 @@ import { useLoaderData } from "react-router-dom"
 
 function DayView(props){
     const data = useLoaderData()
-    console.log(data)
+//CHAR X TIME TABLE
+//STRING TEMPLATE THE STATUS INTO EACH CELLS CLASSNAME FOR COLOR CODING
+//IT SHOULD WORK FOR DAYS W MISSING DATA!
+//    console.log(data)
     return (
         <>
             <h1>DAY VIEW!</h1>
-            <ul>
-            {data.map((element, index) => {
-                for (let i in Object.keys(element.chairs)){
-                    const mountainTime = new Date(data[index].reportDate)
-                    return <li key ={index}>{mountainTime.toLocaleString()}</li>
-                }
-            })}
-            </ul>
+            <table>
+            <tbody>
+                <tr>
+                {data.map((element, index) => {
+                    for (let i in Object.keys(element.chairs)){
+                        const mountainTime = new Date(data[index].reportDate)
+                        return <td key ={index}> {mountainTime.toLocaleString().split(',')[1]}</td>
+                    }
+                })}
+                </tr>
+                </tbody>
+            </table>
         </>
     )
 }
