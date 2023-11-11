b2837dbc4ed52f68d8584a97db17a4fda46b88e1
Alec Hannaford <alec322@gmail.com>
2023-11-11 11:56:30 -0800

    left and right buttons re-route to new month page with the proper date params

--- src/Pages/MonthPage.jsx
+++ src/Pages/MonthPage.jsx
@@ -1,7 +1,29 @@
+//react router or react router dom??
+import { useLoaderData, useParams, Link} from "react-router-dom"
+
+
 export function MonthPage(props){
+    const data = useLoaderData()
+    const {month, year} = useParams()
+    //We are gonna do zero-indexed months here.. and then have the calendar component accept the currentMonthDate
+    const currentMonthDate = new Date(year, month)
+
+    const previousMonthDate = new Date(currentMonthDate)
+    previousMonthDate.setMonth(currentMonthDate.getMonth() - 1)
+
+    const nextMonthDate = new Date(currentMonthDate)
+    nextMonthDate.setMonth(currentMonthDate.getMonth() + 1)
+
     return (
         <>
             <h1>New Month Page</h1>
+            <Link
+            to = {`/showMonth/${previousMonthDate.getMonth()}/${previousMonthDate.getFullYear()}`}
+            ><i>Left</i></Link>
+            <i>{month}/{year}</i>
+            <Link
+            to = {`/showMonth/${nextMonthDate.getMonth()}/${nextMonthDate.getFullYear()}`}
+            ><i>Right</i></Link>
         </>
     )
 }
