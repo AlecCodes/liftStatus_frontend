e4c02fced5bca17e7562baa4def54420643a10f7
Alec Hannaford <alec322@gmail.com>
2023-11-19 19:29:15 -0500

    functional tomorrow button no styling

--- src/Pages/DayView.jsx
+++ src/Pages/DayView.jsx
@@ -1,19 +1,30 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData , useParams, Link} from "react-router-dom"
 
 
 function DayView(props){
     const data = useLoaderData()
+    const {day} = useParams()
 //CHAR X TIME TABLE
 //STRING TEMPLATE THE STATUS INTO EACH CELLS CLASSNAME FOR COLOR CODING
 //IT SHOULD WORK FOR DAYS W MISSING DATA!
 //WHAT IF I made a row component and passed the lift status to it 
 //    console.log(data)
+    const tomorrow = new Date(day)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    console.log(tomorrow.toISOString())
+
+
+
 
     const liftNames = Object.keys(data[0].chairs)
+
 
     return (
         <>
             <h1 className="dayDate">{data[0].reportDate.split('T')[0]}</h1>
+            <Link
+            to = {`../../showDay/${tomorrow.toISOString()}`}
+            >TOMORROW</Link>
             <table>
             <tbody>
                 <tr>
