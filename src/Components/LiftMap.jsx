084f5d2cb79b5d485cb1614814c3bb6e83790a69
Alec Hannaford <alec322@gmail.com>
2024-02-24 22:05:31 -0800

    map lift lines dynamically made with loop

--- src/Components/LiftMap.jsx
+++ src/Components/LiftMap.jsx
@@ -3,19 +3,38 @@ function LiftMap(props){
     const data = props.liftReportData.chairs
     console.log(data)
 
-
+    const chairArray = Object.entries(data)
+    console.log(chairArray)
 
     return(
         <>
             <h1>LIFTMAPP</h1>
             <div className="map-container">
                 <img id = "liftMap" src="https://imgur.com/I4yb0G5.png"/>
-                <div 
+                {/* Could we dynamically create these divs? */}
+                {/* Use Object.entries to create a list of single field objects? */}
+
+                {
+                    chairArray.map((element, index, arr) => {
+
+                    return (
+                        <div
+                        key={index}
+                        id={`${element[1]}`}
+                        className={`${element[0]} chairliftBar`}
+                        ></div>
+                    )
+                    
+                    })                    
+                }
+
+                {/* <div 
                 id={`${data.cloudNine}`} 
                 className={`cloudNine chairliftBar`}></div>
                 <div 
                 id={`${data.chair25}`}
-                className={`chair25 charliftBar`}></div>
+                className={`chair25 charliftBar`}></div> */}
+
             </div>
             
         </>
